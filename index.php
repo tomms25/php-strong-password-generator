dca6eee7b1c4cae279c6699063ca105353b3bd92
tomms25 <tommasobaccano@gmail.com>
2023-01-11 15:50:48 +0100

    Creazione form

---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>php-strong-password-generator</title>
+    <?php
+
+        $passW = $_GET['password'] ?? false;
+
+        var_dump($passW);
+
+    ?>
+
+
+
+
+
+</head>
+<body>
+    <h1>STRONG PASSWORD GENERATOR</h1>
+    <form>
+        <label for="password">Password casuale</label>
+        <input type="text" name="password" placeholder="Inserisci un nuemero">
+        <input type="submit" value="Generate">
+    </form>
+
+
+
+
+
+    
+</body>
+</html>
